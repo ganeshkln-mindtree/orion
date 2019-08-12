3a64f62af303a92e637ce8a3f870cda6d5492671
Ganesh Kumar <ganesh-kum@HCL.COM>
2019-08-12 16:06:59 +0530

    image place holder

--- src/components/movielist/List.js
+++ src/components/movielist/List.js
@@ -1,7 +1,7 @@
 import React from 'react';
  const List = (props) => {
     return (<div className="productGrid">
-        <img src={props.movie.Poster}/>
+        <img src={props.movie.Poster} onError={(e)=>{e.target.onerror = null; e.target.src="logo192.png"}}/>
         <div>Name: {props.movie.Title}</div>
         <div>Year: {props.movie.Year}</div>
         <div>imdbID: {props.movie.imdbID}</div>
